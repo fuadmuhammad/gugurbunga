a3a95bd1a49f8b6639a2712b7da3e55fa0931c37
fuad <fuad1986@gmail.com>
2010-07-04 15:13:05 +0700

    push

--- gugurbunga.rb
+++ gugurbunga.rb
@@ -11,7 +11,7 @@ require "active_record"
 #)
 
 dbconfig = YAML.load(File.read('config/database.yml'))
-ActiveRecord::Base.establish_connection dbconfig['development']
+ActiveRecord::Base.establish_connection dbconfig['production']
 
 
 class Bunga < ActiveRecord::Base
