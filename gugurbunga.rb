4e9b04e7c00b0ed4bee42c769ef021268e852cbf
fuad <fuad1986@gmail.com>
2010-07-04 15:02:14 +0700

    testing

--- gugurbunga.rb
+++ gugurbunga.rb
@@ -24,13 +24,14 @@ end
 post '/upload' do
   CSV::Reader.parse(params[:csv][:tempfile]) do |line|
     if !line[1].nil? && line[1].slice(0,5) == 'Bunga'
-      month_year = Date.strptime(line[0], "%d/%m/%Y")
-      bunga = Bunga.find(:first,:conditions=> ["date::text like :month_year",{:month_year=>"%"+month_year.to_s+"%"}])
+      #month_year = Date.strptime(line[0], "%d/%m/%Y")
+      month_year = line[0]
+      bunga = Bunga.find(:first,:conditions=> ["date = :month_year",{:month_year=>month_year}])
       unless bunga.nil?
         @message =  "Data bunga untuk bulan "+line[0]+"sudah ada"
       else	
         bunga = line[3].gsub(".","").gsub(",",".")
-        Bunga.create(:username=>"fuad",:date=>Date.strptime(line[0], "%d/%m/%Y"),:bunga=>bunga)
+        Bunga.create(:username=>"fuad",:date=>line[0],:bunga=>bunga)
 	@message = "Data berhasil ditambah"
       end
     end
