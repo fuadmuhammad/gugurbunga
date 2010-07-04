9ef311cf5ec46233c324a477f9d715d6ec366815
fuad <fuad1986@gmail.com>
2010-07-04 14:46:18 +0700

    coba

--- gugurbunga.rb
+++ gugurbunga.rb
@@ -25,7 +25,7 @@ post '/upload' do
   CSV::Reader.parse(params[:csv][:tempfile]) do |line|
     if !line[1].nil? && line[1].slice(0,5) == 'Bunga'
       month_year = Date.strptime(line[0], "%d/%m/%Y")
-      bunga = Bunga.find(:first,:conditions=> ["date like :month_year",{:month_year=>"%"+month_year.to_s+"%"}])
+      bunga = Bunga.find(:first,:conditions=> ["date::text like :month_year",{:month_year=>"%"+month_year.to_s+"%"}])
       unless bunga.nil?
         @message =  "Data bunga untuk bulan "+line[0]+"sudah ada"
       else	
